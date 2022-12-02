5f9594a36048b9c1a307dd3f9424239bb97ef827
panjimavi <fjja86@gmail.com>
2022-12-02 18:17:06 -0300

    sentencias

---
+++ sentencias.java
@@ -0,0 +1,48 @@
+package com.mycompany.sentencias;
+
+/**
+ *
+ * @author cisco
+ */
+public class Sentencias {
+
+    public static void main(String[] args) {
+        
+        if(5 > 4){ // mayor que
+            System.out.println("La condicion es verdadera");
+        }else {
+            System.out.println("La condicion es falsa");
+        }
+        
+        if(5 != 4){ // es distindo
+            System.out.println("La condicion es verdadera");
+        }else {
+            System.out.println("La condicion es falsa");
+        }
+        
+        if(5 == 4){ // es igual
+            System.out.println("La condicion es verdadera");
+        }else {
+            System.out.println("La condicion es falsa");
+        }
+        
+        if(5 > 4 && 9 < 2){ // Operador and, debe cumplir ambas condiciones
+            System.out.println("La condicion es verdadera");
+        }else {
+            System.out.println("La condicion es falsa");
+        }
+        
+        if(5 > 4 || 9 < 2){ // Operador or, debe cumplir al menos una de las condiciones
+            System.out.println("La condicion es verdadera");
+        }else {
+            System.out.println("La condicion es falsa");
+        }
+        
+        if(!(5 == 3)){ // Operador not, devuelve como resultado el valor de este invertido
+            System.out.println("La condicion es verdadera");
+        }else {
+            System.out.println("La condicion es falsa");
+        }
+
+    }
+}
