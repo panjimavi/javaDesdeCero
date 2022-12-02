f3225be2b5a05c6fe95748f49fb594bc91814774
panjimavi <fjja86@gmail.com>
2022-12-02 13:12:12 -0300

    primer

---
+++ Primer.java
@@ -0,0 +1,21 @@
+package com.mycompany.primer;
+
+/**
+ * Esta es la manera de crear comentarios en varias lineas
+ * @author cisco
+ */
+
+// Esta es la manera de crear comentarios en una línea 
+
+public class Primer {
+
+    public static void main(String[] args) {
+        System.out.println("Empieza nuestro curso en JAVA!");
+    }
+}
+
+/**
+ * Se puede Imprimir un mensaje con print o println
+ * print: el método print imprimirá el mensaje en la misma línea
+ * println: el método println el mensaje se imprimirá en consola con un salto de línea
+ */
