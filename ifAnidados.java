4b11fbe89ce691f0fa580f0c9b8929b05493a4ff
panjimavi <fjja86@gmail.com>
2022-12-02 18:33:23 -0300

    if anidados

---
+++ ifAnidados.java
@@ -0,0 +1,28 @@
+package com.mycompany.ifanidados;
+
+/**
+ *
+ * @author cisco
+ */
+public class IfAnidados {
+
+    public static void main(String[] args) {
+        
+        int a = 15;
+        int b = 26;
+        int c = 17;
+
+        // if anidado
+        if (a > b) {
+            if (a > c) {
+                System.out.println("A es mayor");
+            } else {
+                System.out.println("C es mayor");
+            }
+        } else if (b > c) {
+            System.out.println("B es mayor");
+        } else {
+            System.out.println("C es mayor");
+        }
+    }
+}
